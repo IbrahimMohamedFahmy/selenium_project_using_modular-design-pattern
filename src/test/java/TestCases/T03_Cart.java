7a46755220a10cefcc6bef8bb1520b8f2c9ec3c5
Ibrahim Fahmy <ibrahimmohamedfahmi1@gmail.com>
2025-11-22 19:29:23 +0200

    add sign in flow "scenarios" and refactor the code

--- src/test/java/TestCases/T03_Cart.java
+++ src/test/java/TestCases/T03_Cart.java
@@ -1,24 +1,51 @@
 package TestCases;
 
 import SetUp.SuperClass;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
 
 public class T03_Cart extends SuperClass
 {
-    @Test(groups = {"Happy Scenarios", "All Scenarios"}, priority = 1)
-    public void AddAProductToCartSuccessfully()
+    @Test(groups = {"Happy Scenarios", "All Scenarios"}, priority = 1, dataProvider = "TestData")
+    public void AddAProductToCartSuccessfully(HashMap<String, String> input)
     {
     };
 
-    @Test(groups = {"Happy Scenarios", "All Scenarios"}, priority = 2)
-    public void AddMoreThanProductToCartSuccessfully()
+    @Test(groups = {"Happy Scenarios", "All Scenarios"}, priority = 2, dataProvider = "TestData")
+    public void AddMoreThanProductToCartSuccessfully(HashMap<String, String> input)
     {
     };
 
-    @Test(groups = {"Happy Scenarios", "All Scenarios"}, priority = 3)
-    public void AddAProductToCartFromHisPageSuccessfully()
+    @Test(groups = {"Happy Scenarios", "All Scenarios"}, priority = 3, dataProvider = "TestData")
+    public void AddAProductToCartFromHisPageSuccessfully(HashMap<String, String> input)
     {
     };
+
+    @DataProvider
+    public Object[][] TestData() throws IOException
+    {
+        String jsonFile = FileUtils.readFileToString(new File(System.getProperty("user.dir")+"//src//main//resources//TestData.json"), StandardCharsets.UTF_8);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<HashMap<String, String>> DataList = objectMapper.readValue(jsonFile, new TypeReference<List<HashMap<String, String>>>() {});
+
+        Object[][] Data = new Object[DataList.size()][1];
+        for(int i = 0 ; i < DataList.size() ; i++)
+        {
+            Data[i][0] = DataList.get(i);
+        }
+        return Data;
+
+    }
 
     /*
     context("Sad Scenarios", function()
